2c3471112eb0ef87448db2cd91ba9432348c08d9
sebpivot <seb.pivot@live.fr>
2019-11-21 22:31:33 +0100

    delete of the auth component, not used anymore, and change of app.module regarding this suppression

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { AuthComponent } from './auth/auth.component';
 import {RouterModule, Routes} from '@angular/router';
-import {AuthService} from './services/auth.service';
 import {HttpClientModule} from '@angular/common/http';
 import { StudentDetailsComponent } from './student-details/student-details.component';
 import {StudentService} from './services/student.service';
@@ -19,10 +17,7 @@ import { MainPageComponent } from './main-page/main-page.component';
 
 const appRoutes: Routes = [
 
-  { path: 'auth', component: AuthComponent },
   { path: 'students', component: StudentListViewComponent},
-  { path: 'students/:id', component: StudentDetailsComponent},
-  { path: 'studentList', component: StudentListViewComponent},
   { path: 'students/:id', component: StudentDetailsComponent},
   { path: 'teachers', component: ProfListViewComponent},
   { path: 'teachers/:id', component: ProfDetailsComponent},
@@ -34,7 +29,6 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent,
     StudentListViewComponent,
     StudentDetailsComponent,
     ProfListViewComponent,
@@ -50,7 +44,6 @@ const appRoutes: Routes = [
 /*    HttpClientModule*/
   ],
   providers: [
-    AuthService,
     StudentService,
     TeacherService
   ],
