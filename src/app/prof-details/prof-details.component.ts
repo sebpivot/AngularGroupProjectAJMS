d72a49f06fafc406adb39129cdaf56de37826a6f
sebpivot <seb.pivot@live.fr>
2019-11-25 15:50:15 +0100

    Addition of the details of the teachers with a button detail when the list is opened

--- src/app/prof-details/prof-details.component.ts
+++ src/app/prof-details/prof-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {TeacherService} from '../services/teacher.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Teacher} from '../model/Teacher';
 
 @Component({
   selector: 'app-prof-details',
@@ -9,16 +10,27 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ProfDetailsComponent implements OnInit {
 
-  teacherFirstname;
-  teacherLastname;
+ id: number;
+ teacher: Teacher;
 
   constructor(private teacherService: TeacherService,
+              private router: Router,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id = this.route.snapshot.params.id;
-    this.teacherFirstname = this.teacherService.getTeacherById(+id).Firstname;
-    this.teacherLastname = this.teacherService.getTeacherById(+id).Lastname;
+    this.teacher = new Teacher();
+
+    this.id = this.route.snapshot.params.id;
+
+    this.teacherService.getTeacher(this.id)
+      .subscribe(data => {
+      console.log(data);
+      this.teacher = data;
+    }, error => console.log(error));
+}
+
+  list() {
+    this.router.navigate(['teachers']);
   }
 
 }
